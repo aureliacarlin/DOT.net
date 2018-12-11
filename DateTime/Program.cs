bfc53d2c1fa03e5757c0f9ee6f69399ea9cd1c1e
Aurelia Carlin <aricarlin@gmail.com>
2018-12-11 13:27:33 -0500

    DateTime/Interfaces

---
+++ DateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime value = new DateTime(2018, 1, 15)
+            Console.WriteLine(value);
+
+        }
+    }
+}
